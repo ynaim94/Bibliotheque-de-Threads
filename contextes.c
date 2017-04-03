99f9bc13dad3e47fd769b10b4bd33e65e24da35d
VIKSTROM Hugo <hvikstrom@diagnostix.pedago.ipb.fr>
2017-04-03 12:11:07 +0200

    bla

--- contextes.c
+++ contextes.c
@@ -2,9 +2,23 @@
 #include <stdlib.h>
 #include <ucontext.h> /* ne compile pas avec -std=c89 ou -std=c99 */
 
+
+void other(char* name){
+  printf("le nom est %s\n", name);
+}
+
 void func(int numero)
 {
+  ucontext_t test, previous;
+
+  getcontext(&test);
+  test.uc_stack.ss_size = 64*1024;
+  test.uc_stack.ss_sp = malloc(test.uc_stack.ss_size);
+  test.uc_link = &previous;
+  makecontext(&test, (void (*)(void)) other,1, "bla");
+  swapcontext(&previous, &test);
   printf("j'affiche le numéro %d\n", numero);
+  
 }
 
 int main() {
@@ -12,7 +26,7 @@ int main() {
 
   getcontext(&uc); /* initialisation de uc avec valeurs coherentes
 		    * (pour éviter de tout remplir a la main ci-dessous) */
-
+  getcontext(&previous);
   uc.uc_stack.ss_size = 64*1024;
   uc.uc_stack.ss_sp = malloc(uc.uc_stack.ss_size);
   uc.uc_link = &previous;
@@ -26,7 +40,6 @@ int main() {
   uc.uc_stack.ss_sp = malloc(uc.uc_stack.ss_size);
   uc.uc_link = NULL;
   makecontext(&uc, (void (*)(void)) func, 1, 57);
-
   printf("je suis dans le main\n");
   setcontext(&uc);
   printf("je ne reviens jamais ici\n");
