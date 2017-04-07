07eab281a9cff74a38de649d59547cc77f552322
VIKSTROM Hugo <hugo.vikstrom@enseirb-matmeca.fr>
2017-04-07 23:45:18 +0200

    complétion incertaine de thread_join

--- thread.c
+++ thread.c
@@ -2,14 +2,14 @@
 #include "queue.h"
 #include <ucontext.h>
 
-struct thread{
-  thread_t id;
-  ucontext_t context;
-};
-
+typedef void* ret;
 SIMPLEQ_HEAD(queue, thread);
 struct queue head;
-SIMPLEQ_INIT(head);
+struct thread {
+  SIMPLEQ_ENTRY(thread_t) id;
+  SIMPLEQ_ENTRY(ucontext_t) context;
+  SIMPLEQ_ENTRY(ret) retval;
+} *t1, *t2;
 
 
 thread_t thread_self(void){
@@ -22,6 +22,36 @@ int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
 
 int thread_yield(void){}
 
-int thread_join(thread_t thread, void **retval){}
+/* Permet de placer la valeur de retour du thread spécifié à l'adresse retval */
 
-void thread_exit(void *retval) __attribute__ ((__noreturn__)){}
+int thread_join(thread_t thread, void **retval){
+  thread loop;
+  thread_t tmp=NULL;
+  thread current;
+  SIMPLEQ_FOREACH(loop, head, id){
+    if (loop.id==thread){ //on cherche si thread est bien dans la liste
+      printf("il existe !%p\n", thread);
+      tmp=thread;
+    }
+    if (loop.id==thread_self()){ //on veut récupérer les infos du thread courant
+      current=loop;
+    }
+  }
+  if(tmp==NULL){
+    printf("%p n'appartient pas à la liste\n", thread);
+    return -1;
+  }
+
+  /* trouver un moyen d'attendre que thread ait fini */
+  
+  if(retval == NULL){// si retval est NULL, ne rien faire, sinon ..
+    return 1;
+  }
+  else {
+    *retval=tmp.retval; //..on place la valeur de retour du thread dans retval
+  }
+}
+
+/* simplement placer retval dans le retval du thread self */
+
+void thread_exit(void *retval){}
