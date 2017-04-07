3e0cc003dc4615b8fd6dfa8a057d40caaa6e832a
LE BRETON Guillaume <glebreton@goudurix.pedago.ipb.fr>
2017-04-07 14:41:56 +0200

    vbedafioa

--- thread.c
+++ thread.c
@@ -1,1 +1,16 @@
 #include "thread.h"
+
+struct thread{
+  thread_t id;
+  ucontext_t context;
+};
+
+thread_t thread_self(void){}
+
+int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){}
+
+int thread_yield(void){}
+
+int thread_join(thread_t thread, void **retval){}
+
+void thread_exit(void *retval) __attribute__ ((__noreturn__)){}
