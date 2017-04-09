5a1f5fbb3e390d81d8c2b063da1c76ff698706da
Nouur <nour._.grati@hotmail.fr>
2017-04-09 23:41:32 +0200

    Implémentation de l'interface thread.h en cours

--- thread.c
+++ thread.c
@@ -12,15 +12,39 @@ struct thread {
 } *t1, *t2;
 
 
+ struct thread current_thread ;
+
 thread_t thread_self(void){
-  
+return current_thread.id;
 }
 
 int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
-  
+ ucontext_t ctx;
+struct thread th;	
+  getcontext(&ctx);
+  ctx.uc_stack.ss_size = 64*1024;
+  ctx.uc_stack.ss_sp = malloc(ctx.uc_stack.ss_size);
+  ctx.uc_link = NULL;
+   makecontext(&ctx, (void (*)(void)) func ,1,  *funcarg);
+
+th.id=*newthread;
+th.context=ctx;
+th.retval=0;
+SIMPLEQ_INSERT_TAIL(head, th, field???);
 }
 
-int thread_yield(void){}
+int thread_yield(void){
+		
+
+		if ( SIMPLEQ_EMPTY(head) == NULL ) return;
+		SIMPLEQ_INSERT_TAIL(head, current_thread, field???);
+		/* Free the current_thread's stack */
+		free( current_thread.context.uc_stack.ss_sp);
+		current_thread=SIMPLEQ_FIRST(head);
+		SIMPLEQ_REMOVE_HEAD(head, field??);
+			
+		
+}
 
 /* Permet de placer la valeur de retour du thread spécifié à l'adresse retval */
 
