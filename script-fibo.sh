92cd767101878b7fd757c568923b53a1d9701a2d
VIKSTROM Hugo <hugo.vikstrom@enseirb-matmeca.fr>
2017-05-05 17:13:32 +0200

    ajout gnuplot

---
+++ script-fibo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+    echo "Usage : ./fibonacci i avec i entier"
+    exit;
+fi
+
+if [ -f time.txt ]
+then
+    rm time.txt
+fi
+
+for i in `seq 1 $1`
+do
+
+    { time ./fibonacci $i 2> fibonacci.stderr ; } 2> tmp.txt
+    TIME=`cat tmp.txt | grep real | awk '{print $2}' | sed 's/[a-z]*//g' | cut -c 2-`
+    echo "$i $TIME" >> time.txt
+done
+
+gnuplot graph.conf
